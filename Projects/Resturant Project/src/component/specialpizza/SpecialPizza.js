14e0580b1026b77634e8d02c67c178f6cb146edf
Muhammad Usman <mohammadusman7864@gmail.com>
2022-07-31 07:26:08 +0500

    'ok'

--- Projects/Resturant Project/src/component/specialpizza/SpecialPizza.js
+++ Projects/Resturant Project/src/component/specialpizza/SpecialPizza.js
@@ -9,7 +9,25 @@ const SpecialPizza = ({x}) => {
       <Card.Body>
         <Card.Title>{x.name}</Card.Title>
         <Card.Text>
-        
+        <Row>
+          <Col md={6}>
+          <h6> Varients</h6>
+            <select name="pizza">
+              {x.varients.map((varient)=> (
+                <option value={varient}>{varient}</option>
+              ) )
+              }
+            </select>
+          </Col>
+          <Col md={6}>
+            <h6>Quantity</h6>
+            {[...Array(10).keys()].map((v,i) => 
+               (
+                <option value={i+1}>{i+1}</option>
+               )
+            )}
+          </Col>
+        </Row>
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
