deb94984f050546301e7b77b23daf48ac0031706
Muhammad Usman <mohammadusman7864@gmail.com>
2022-08-01 06:26:37 +0500

    'ok'

--- Projects/Resturant Project/src/component/specialpizza/SpecialPizza.js
+++ Projects/Resturant Project/src/component/specialpizza/SpecialPizza.js
@@ -1,16 +1,28 @@
 import React, {useState} from 'react'
-import {Button,Card, Row, Col} from 'react-bootstrap';
+import {Button,Card, Row, Col, Modal } from 'react-bootstrap';
 import '../specialpizza/SpecialPizza.css'
+
+
 
 const SpecialPizza = ({x}) => {
   const [varient, setVarient] = useState('large')
   const [quantity, setQuantity] = useState(1)
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
     <Card style={{ width: '18rem',  marginTop: '10%', marginBottom:'5%' }}>
-      <Card.Img variant="top" style={{height:'256px'}} src={x.image} />
+      <Card.Img variant="top" 
+      style={{height:'256px', cursor:'pointer'}} 
+      src={x.image} 
+      onClick={handleShow}
+      />
       <Card.Body>
-        <Card.Title>{x.name}</Card.Title>
+        <Card.Title style={{color: '#e06377'}} >{x.name}</Card.Title>
+        <hr/>
         <Card.Text>
         <Row>
           <Col md={6} lg={6}>
@@ -26,7 +38,7 @@ const SpecialPizza = ({x}) => {
           <Col md={6} lg={6}>
             <h6>Quantity</h6>
             <select value={quantity} onChange={(e) => setQuantity(e.target.value)} >
-            {[...Array(10).keys()].map((v,i) => 
+            {[...Array(10).keys()].map((i) => 
                (
                 <option value={i+1} >{i+1}</option>
                )
@@ -45,6 +57,27 @@ const SpecialPizza = ({x}) => {
         </Row>
       </Card.Body>
     </Card>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{x.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+         <div> 
+      <Card.Img variant="top" 
+      style={{height:'256px'}} 
+      src={x.image} />
+      </div>
+      <div style={{marginTop:'2%', color: '#c83349'}} >
+        <h2>Description:</h2>
+      </div>
+      <div>
+        <h4>{x.description}</h4>
+      </div>
+      </Modal.Body>
+       
+      </Modal>
+   
  
     </>
   )
