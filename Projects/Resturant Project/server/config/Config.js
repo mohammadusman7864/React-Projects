730ee18f292cf962ffc5e4be5f338f048761a665
Muhammad Usman <mohammadusman7864@gmail.com>
2022-08-02 09:40:10 +0500

    'ok

---
+++ Projects/Resturant Project/server/config/Config.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+require('color')
+
+const MongoDB= async() => {
+    try {
+
+          const url = process.env.MONGO_URI
+          const conn=    await mongoose.connect(url,{
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+          });
+          console.log(
+            'Mongo Database Connected! ${.con.connection.host}' .bgCyan.white
+          );
+      } catch (error) {
+        console.log('error: ${error.message}' .bgRed.white  );
+      }
+}
+module.exports = MongoDB
